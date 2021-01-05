636777fe7763b2f234fdd51af6532b405623c382
Crowdbotics <team@crowdbotics.com>
2021-01-05 09:36:55 +0000

    Update models

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.urls import reverse
@@ -5,10 +6,14 @@ from django.utils.translation import ugettext_lazy as _
 
 
 class User(AbstractUser):
+    name = models.CharField(
+        null=True,
+        blank=True,
+        max_length=255,
+    )
 
-    # First Name and Last Name do not cover name patterns
-    # around the globe.
-    name = models.CharField(_("Name of User"), blank=True, null=True, max_length=255)
+    def get_absolute_url(self):
+        return reverse("users:detail", kwargs={"username": self.username})
 
     def get_absolute_url(self):
         return reverse("users:detail", kwargs={"username": self.username})
